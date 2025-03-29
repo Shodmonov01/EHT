9f0e7935b9c6c739bc39dd18fb99274492651fac
cap-ma <m.xasanov@uic.group>
2025-03-29 18:43:06 +0500

    udpate url

--- backend/quiz/urls.py
+++ backend/quiz/urls.py
@@ -7,9 +7,9 @@ from .views import (
 urlpatterns = [
     path('api/category-set/', CategorySetListAPIView.as_view(), name='category_set_list'),
     path('api/quizzes/start', StartQuizAPIView.as_view(), name='quiz-create'),
-    path('api/quizzes/<int:quiz_id>/', QuizDetailAPIView.as_view(), name='api_quiz_detail'),
-    path('api/quizzes/<int:quiz_id>/submit/', SubmitQuizAPIView.as_view(), name='api_submit_quiz'),
-    path('api/quiz_result/<int:quiz_result_id>/', QuizResultAPIView.as_view(), name='api_quiz_result'),
-    path('api/summary/<int:quiz_result_id>/', SummaryAPIView.as_view(), name='api_summary'),
-    path('api/table/<int:quiz_result_id>/', TableAPIView.as_view(), name='api_table'),
+    # path('api/quizzes/<int:quiz_id>/', QuizDetailAPIView.as_view(), name='api_quiz_detail'),
+    # path('api/quizzes/<int:quiz_id>/submit/', SubmitQuizAPIView.as_view(), name='api_submit_quiz'),
+    # path('api/quiz_result/<int:quiz_result_id>/', QuizResultAPIView.as_view(), name='api_quiz_result'),
+    # path('api/summary/<int:quiz_result_id>/', SummaryAPIView.as_view(), name='api_summary'),
+    # path('api/table/<int:quiz_result_id>/', TableAPIView.as_view(), name='api_table'),
 ]
