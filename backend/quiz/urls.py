baf543e822636499539a850936d706ca7325e3b4
cap-ma <m.xasanov@uic.group>
2025-03-31 14:47:12 +0500

    updated quizlist api from modelviewset

--- backend/quiz/urls.py
+++ backend/quiz/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-     CategorySetListAPIView, StartQuizAPIView, QuizViewSet,  QuizResultViewSet
+     CategorySetListAPIView, StartQuizAPIView, QuizListView, QuizQuestionsView,  QuizResultViewSet
 )
 
 router = DefaultRouter()
-router.register(r'questions', QuizViewSet)
+
 router.register(r'quiz-results', QuizResultViewSet)
 
 urlpatterns = [
     path('api/category-set/', CategorySetListAPIView.as_view(), name='category_set_list'),
     path('api/quizzes/start', StartQuizAPIView.as_view(), name='quiz-create'),
-    path('api/quizzes/', include(router.urls))
+    path('api/quizzes/', include(router.urls)),
+    path('quizzes/', QuizListView.as_view(), name='quiz-list'),
+    path('quizzes/<int:quiz_id>/questions/', QuizQuestionsView.as_view(), name='quiz-questions'),
     # path('api/quizzes/<int:quiz_id>/', QuizDetailAPIView.as_view(), name='api_quiz_detail'),
     # path('api/quizzes/<int:quiz_id>/submit/', SubmitQuizAPIView.as_view(), name='api_submit_quiz'),
     # path('api/quiz_result/<int:quiz_result_id>/', QuizResultAPIView.as_view(), name='api_quiz_result'),
