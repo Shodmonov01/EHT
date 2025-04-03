c224ac6726940560a67ab1537f2b3da3e8696f27
cap-ma <m.xasanov@uic.group>
2025-04-03 22:04:37 +0500

    update pdf final

--- backend/quiz/admin.py
+++ backend/quiz/admin.py
@@ -17,7 +17,7 @@ class QuestionAdmin(nested_admin.NestedModelAdmin):
     ordering = [ 'theme__category__name', 'theme__name', 'text']
     search_fields = ['text',]
     list_per_page = 50
-    list_display = ['text', 'quiz_title', 'category_name']
+    list_display = ['text']
     raw_id_fields = ['theme']
     inlines = [AnswerInline]
 
