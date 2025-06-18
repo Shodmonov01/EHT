db2454b7e58d3cee093102976486b2f3dddb2386
cap-ma <m.xasanov@uic.group>
2025-06-18 21:43:09 +0500

    change i18

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -40,6 +40,11 @@ class Specialization(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name = _("Specialization")
+        verbose_name_plural = _("Specializations")
+
 
 
 
