9639f82f5c7e45e0507279677ffbda25b71e2f90
Shodmonov01 <a.shodmonov60@gmail.com>
2025-06-22 20:34:07 +0500

    Clean commit without large files

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL;
+import axios from 'axios'
+const API_URL = import.meta.env.VITE_API_BASE_URL
 export const fetchData = async () => {
-  const response = await axios.get(`${API_URL}`);
-  return response.data;
-};
+    const response = await axios.get(`${API_URL}`)
+    return response.data
+}
